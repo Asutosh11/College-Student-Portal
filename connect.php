ea8c987b252774d89b32ba00662f3142dc39f16f
Asutosh Panda <panda.asutosh04@gmail.com>
2015-06-28 10:50:48 +0530

    some changes

--- connect.php
+++ connect.php
@@ -3,14 +3,14 @@
     date_default_timezone_set('Asia/Calcutta');
     
     
-    $host = 'localhost' ; // Enter your host name in the quotes. If your database is on the same server, it will be localhost
+    $host = '' ; // Enter your host name in the quotes. If your database is on the same server, it will be localhost
     
-    $username = 'asu619' ; // Enter your database username in the quotes.
+    $username = '' ; // Enter your database username in the quotes.
     
-    $password = 'ping49tw' ; // Enter your database password in the quotes.
+    $password = '' ; // Enter your database password in the quotes.
     
     
-    $database_name = 'collegecms' ;  // Enter your database name in the quotes.
+    $database_name = '' ;  // Enter your database name in the quotes.
     
     
     $dbc = mysqli_connect($host, $username, $password, $database_name) or die('Error connecting to databse') ;
